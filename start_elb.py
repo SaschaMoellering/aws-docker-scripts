0510b99b122bbe76b8e904f21b38f83ccaf84cf1
SaschaMoellering <sascha.moellering@zanox.com>
2014-08-22 14:43:59 +0200

    Added missing MetricAlarms

--- start_elb.py
+++ start_elb.py
@@ -1,10 +1,12 @@
 import boto.ec2
+import boto.ec2.cloudwatch
 
 from boto.ec2.elb import HealthCheck
 
 from boto.ec2.autoscale import LaunchConfiguration
 from boto.ec2.autoscale import AutoScalingGroup
 from boto.ec2.autoscale import ScalingPolicy
+from boto.ec2.cloudwatch import MetricAlarm
 
 import sys
 import getopt
@@ -157,7 +159,7 @@ def start_elb(tag, user_data):
     scaling_up_policy = ScalingPolicy(name=elb_tag + "webserverScaleUpPolicy",
                                       adjustment_type='ChangeInCapacity',
                                       as_name=ag.name,
-                                      scaling_adjustment=2,
+                                      scaling_adjustment=1,
                                       cooldown=180)
 
     scaling_down_policy = ScalingPolicy(name=elb_tag + "webserverScaleDownPolicy",
@@ -168,6 +170,33 @@ def start_elb(tag, user_data):
 
     conn_as.create_scaling_policy(scaling_up_policy)
     conn_as.create_scaling_policy(scaling_down_policy)
+
+    scaling_up_policy = conn_as.get_all_policies(
+            as_group=elb_tag + "webserverScaleUpPolicy", policy_names=['scale_up'])[0]
+    scaling_down_policy = conn_as.get_all_policies(
+            as_group=elb_tag + "webserverScaleDownPolicy", policy_names=['scale_down'])[0]
+
+    cloudwatch = boto.ec2.cloudwatch.connect_to_region(region)
+    alarm_dimensions = {"AutoScalingGroupName": 'my_group'}
+
+    scale_up_alarm = MetricAlarm(
+            name='scale_up_on_cpu', namespace='AWS/EC2',
+            metric='CPUUtilization', statistic='Average',
+            comparison='>', threshold='70',
+            period='60', evaluation_periods=2,
+            alarm_actions=[scaling_up_policy.policy_arn],
+            dimensions=alarm_dimensions)
+
+    scale_down_alarm = MetricAlarm(
+            name='scale_down_on_cpu', namespace='AWS/EC2',
+            metric='CPUUtilization', statistic='Average',
+            comparison='<', threshold='40',
+            period='60', evaluation_periods=2,
+            alarm_actions=[scaling_down_policy.policy_arn],
+            dimensions=alarm_dimensions)
+
+    cloudwatch.create_alarm(scale_down_alarm)
+    cloudwatch.create_alarm(scale_up_alarm)
 
 
 if __name__ == "__main__":
